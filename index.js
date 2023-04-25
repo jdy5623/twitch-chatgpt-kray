79d5825464af87aa7f457bf20341ed81df798269
matteo335 <79221584+matteo335@users.noreply.github.com>
2023-04-25 03:59:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const { promisify } = require('util')
 const readFile = promisify(fs.readFile)
 const GPT_MODE = process.env.GPT_MODE
+const port = process.env.PORT || 3000
 
 let file_context = "You are a helpful Twitch Chatbot."
 
