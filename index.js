72b031d1a052cf37259f1291c6b16cb05b22f819
matteo335 <79221584+matteo335@users.noreply.github.com>
2023-04-25 03:59:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -99,36 +99,5 @@ app.get('/gpt/:text', async (req, res) => {
       } else {
         res.send("Something went wrong. Try again later!")
       }
-
-    } else {
-      //PROMPT MODE EXECUTION
-      const prompt = file_context + "\n\nQ:" + text + "\nA:";
-      console.log("User Input: " + text)
-
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        temperature: 0.5,
-        max_tokens: 128,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0,
-      });
-      if (response.data.choices) {
-        let agent_response = response.data.choices[0].text
-          console.log ("Agent answer: " + agent_response)
-          //Check for Twitch max. chat message length limit and slice if needed
-          if(agent_response.length > 399){
-            console.log("Agent answer exceeds twitch chat limit. Slicing to first 399 characters.")
-            agent_response = agent_response.substring(0, 399)
-            console.log ("Sliced Agent answer: " + agent_response)
-          }
-
-          res.send(agent_response)
-      } else {
-          res.send("Something went wrong. Try again later!")
-      }
-    }
-    
 })
 app.listen(process.env.PORT || 3000)
